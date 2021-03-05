5dc58a4a48e7bed96e887b6d85a07c2ba6b2fd95
franknoh <42113651+franknoh@users.noreply.github.com>
2021-03-05 12:10:44 +0900

    0.3v 추가
    0.3v추가, bug fix

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 Nuna = require('./nuna');
-Nuna.init();
 
-// test.nuna
-// output: `누나`
-console.log(Nuna.exec(`test`));
-console.log(Nuna.exec(`./test`));
-console.log(Nuna.exec(`test.nuna`));
-console.log(Nuna.exec(`./test.nuna`));
+Nuna.init('0.2');
+console.log(Nuna.exec(`v0.2`))
 
-// text
-// output: `누나`
-console.log(Nuna.exec(`
-눈나..나..주...나..........거나..........거....나..........거나..........거나....누........나.........💕
-누나..나..나..거나..나.....나.....거...나..나.....거나..나.....주..눈나..........나..........💕!
-`));
+Nuna.init('0.3');
+console.log(Nuna.exec(`v0.3`))
 
+console.log(Nuna.list())
+
+console.log(Nuna.ver)
